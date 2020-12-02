3eb6371b7dba6dc02e7bdb9a2051e4ee077d45b9
SilvanK4t1qbit <silvan.kuttimalai@1qbit.com>
2020-12-01 20:41:40 -0800

    tmp

--- day-1/play.lisp
+++ day-1/play.lisp
@@ -1,7 +1,15 @@
 #!/usr/bin/env sbcl --script
 
+  
+
 (let ((infile (open "input" :if-does-not-exist nil)))
   (when infile
-    (format t "~a~%" (read-line infile))
+    (defvar inlist (loop :for line := (read-line infile nil)
+			 :while line
+			 :collect line))
+    (write inlist)
+    ;; (while list
+    ;;   (print (car list))
+    ;;   (setq list (cdr list)))
     (close infile)))
 
