f44792bc95b5b72e00e97641222e104ec5a7dfae
SilvanK4t1qbit <silvan.kuttimalai@1qbit.com>
2020-12-01 20:58:20 -0800

    New

--- day-1/play.lisp
+++ day-1/play.lisp
@@ -4,12 +4,9 @@
 
 (let ((infile (open "input" :if-does-not-exist nil)))
   (when infile
-    (defvar inlist (loop :for line := (read-line infile nil)
-			 :while line
-			 :collect line))
-    (write inlist)
-    ;; (while list
-    ;;   (print (car list))
-    ;;   (setq list (cdr list)))
+    (let ((inlist (loop :for line := (read-line infile nil)
+		  :while line
+		  :collect line)))
+      (write inlist))
     (close infile)))
 
