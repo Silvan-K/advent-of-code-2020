65dc76bd0f439001edbb7e304ff6ae053bebcab4
SilvanK4t1qbit <silvan.kuttimalai@1qbit.com>
2020-12-02 07:56:38 -0800

    Add sum check function

--- day-1/play.lisp
+++ day-1/play.lisp
@@ -8,6 +8,9 @@
 (defun parse-numbers (strings)
   (mapcar #'parse-integer strings))
 
+(defun sum-to-2020 (x y)
+  (= 2020 (+ x y)))
+
 (let ((infile (open "input" :if-does-not-exist nil)))
   (let ((numbers (parse-numbers (read-lines infile))))
     (write numbers))
