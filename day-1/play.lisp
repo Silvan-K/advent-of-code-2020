8a9cd00bfabe722a785a02a230cbcf209cbf75da
SilvanK4t1qbit <silvan.kuttimalai@1qbit.com>
2020-12-02 16:08:11 -0800

    Finished!

--- day-1/play.lisp
+++ day-1/play.lisp
@@ -16,5 +16,17 @@
 (defun sum-to-2020 (x y)
   (= 2020 (+ x y)))
 
+(defun find-2020-partner (num0 numbers)
+  (dolist (num1 numbers)
+    (when (sum-to-2020 num0 num1)
+      (return-from find-2020-partner num1))))
+
+(defun find-2020-pair (numbers)
+  (dolist (num0 numbers)
+    (let ((num1 (find-2020-partner num0 numbers)))
+      (when num1
+	(return-from find-2020-pair (cons num0 num1))))))
+
 (let ((numbers (get-numbers "input")))
-  (write numbers))
+  (let ((pair (find-2020-pair numbers)))
+    (write (* (car pair) (cdr pair)))))
